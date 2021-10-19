5251a262574511d3fa5d0413f748d4658b7def33
Rikard Virta <rikard.virta@dahl.se>
2021-10-19 13:48:36 +0200

    minor edits

--- pages/components-demo.vue
+++ pages/components-demo.vue
@@ -2,7 +2,7 @@
   <section>
     <h1>Components demo</h1>
     <p>Nuxt will scan the /components directory for .vue files and automatically import them into all .vue files.</p>
-    <p>To scan other directories such as /components/webshop, </p>
+    <p>To scan other directories such as /components/webshop additional configuration is required in nuxt.config.js</p>
     <hr class="my-10">
     <DemoComponent />
     <hr class="my-10">
--- pages/vuebasics-demo.vue
+++ pages/vuebasics-demo.vue
@@ -1,12 +1,18 @@
 <template>
   <section>
     <h1 class="exampleClass">Vue.js basics</h1>
-    <h2>More details...</h2>
-    <v-card v-if="person" class="ma-10">
-      <v-card-title>{{person.name}}</v-card-title>
-      <v-card-text>{{person.height}} cm tall</v-card-text>
-      <v-card-text>{{person.url}}</v-card-text>
-    </v-card>
+    <hr class="my-10">
+    <v-row v-if="person" class="mt-4">
+        <v-col cols="4">
+          <v-hover v-slot="{ hover }">
+            <v-card :elevation="hover ? 24 : 6">
+              <v-card-title>{{person.name}}</v-card-title>
+              <v-card-text>{{person.height}} cm tall</v-card-text>
+              <v-card-text>{{person.url}}</v-card-text>
+            </v-card>
+          </v-hover>
+        </v-col>
+    </v-row>
     <hr class="my-10">
     <p>{{ message }}</p>
     <MyButton @click="handleClick" />
@@ -20,16 +26,6 @@ export default {
   components: {
     MyButton
   },
-  async mounted() {
-    const response = await fetch(`https://swapi.dev/api/people/1`)
-    const data = await response.json()
-    const foundPerson = {
-      name: data.name,
-      height: data.height,
-      url: data.url,
-    }
-    this.person = foundPerson
-  },
   data() {
     return {
       message: 'Hello world',
@@ -40,7 +36,17 @@ export default {
     handleClick() {
       this.message = 'Goodbye world'
     }
-  }
+  },
+  async mounted() {
+    const response = await fetch(`https://swapi.dev/api/people/1`)
+    const data = await response.json()
+    const foundPerson = {
+      name: data.name,
+      height: data.height,
+      url: data.url,
+    }
+    this.person = foundPerson
+  },
 }
 </script>
 
